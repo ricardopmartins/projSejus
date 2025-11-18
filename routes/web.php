ea2b68f6d5c55a5a4d3da9996df3de51c2ca74c6
ricardopmartins <ricardopmartins.gt.pvh@gmail.com>
2025-11-18 12:30:51 -0400

    Consertando alguns bugs e adicionando início da página sobre nós

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('games');
+});
+
+Route::get('/aboutUs', function () {
+    return view('aboutUs');
 });
